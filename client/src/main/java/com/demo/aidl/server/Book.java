35fc732713b807ecda98549d50dab41337f85973
gxd <guoxiaodong@haqu.com>
2020-07-30 19:24:04 +0800

    nothing

--- client/src/main/java/com/demo/aidl/server/Book.java
+++ client/src/main/java/com/demo/aidl/server/Book.java
@@ -8,6 +8,9 @@ import android.os.Parcelable;
  */
 public class Book implements Parcelable {
     public static final Creator<Book> CREATOR = new Creator<Book>() {
+        /**
+         * 从序列化后的对象中创建原始对象
+         */
         @Override
         public Book createFromParcel(Parcel source) {
             return new Book(source);
@@ -36,6 +39,11 @@ public class Book implements Parcelable {
         return 0;
     }
 
+    /**
+     * 将当前对象写入序列化结构中
+     *
+     * @param flags 为1时标识当前对象需要作为返回值返回，不能立即释放资源，几乎所有情况下都为0.
+     */
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(this.name);
